c8cee99e25dbb9eb84248d39ba72f122e10248c5
kskim <kksu964@naver.com>
2020-11-22 11:28:05 +0900

    mqtt test

--- testMap.py
+++ testMap.py
@@ -1,23 +1,18 @@
 # pip install paho-mqtt
 import paho.mqtt.client as mqtt
 import json
+#from mysqlText import *
 import socket
 HOST = '127.0.0.1'  #server address
 PORT = 9640         # 서버에서 지정해 놓은 포트 번호입니다.
 
-# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client_socket.connect((HOST, PORT))
-# msg={"lat":36.396314,, "lng":127.352202
-
-# client_socket.sendall(json.dumps(msg).encode("utf-8"))# 메시지를 전송합니다.
-# client_socket.close()
-
 def on_connect(client, userdata, flags, rc):
-    print("connected", rc)
     if rc == 0:
         print("connected OK")
     else:
         print("Bad connection Returned code=", rc)
+
+
 
 
 def on_disconnect(client, userdata, flags, rc=0):
@@ -30,11 +25,7 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, msg):
     print('received msg:'+str(msg.payload.decode("utf-8")))
-    #msgObj=json.loads(msg.payload)
-    print('msgObj', msg.payload);
-    # client_socket.sendall(msg.payload)  # 메시지를 전송합니다.
-    # print('send', msg.payload)
-    # client_socket.close()
+    msgObj=json.loads(msg.payload)
 
 
 client = mqtt.Client()
@@ -45,6 +36,12 @@ client.on_subscribe = on_subscribe
 client.on_message = on_message
 
 client.connect('5.196.95.208', 1883) #test.mosquitto.org: 5.196.95.208, mqtt://broker.hivemq.com=18.185.228.239 and 3.120.11.85
-client.subscribe('server9650', 1)
-
-client.loop_forever()
+#client.subscribe('server965', 1)
+client.loop_start()
+sendMsg={'lat':36.396314, 'lng':127.352202}
+msg=json.dumps(sendMsg)
+print('sendmsg', sendMsg,'msg', msg, msg.encode())
+client.publish('server9650', "Hello World",1);
+client.loop_start()
+client.disconnect()
+#client.loop_forever()
